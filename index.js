fc26ab48df01584fdec72bec89adc24d02837924
GOPI KUMARGUPTA <122801349+gopikumargupta@users.noreply.github.com>
2023-09-08 13:44:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const countValue=document.getElementById('count');
+const inc =()=>{
+    let value =parseInt(countValue.innerText)
+    value=value+1;
+    countValue.innerText=value
+
+}
+const dec=()=>{
+    let value =parseInt(countValue.innerText)
+    value=value-1;
+    countValue.innerText=value;
+
+}
